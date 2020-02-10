24501e4575a97a81bf00892b967fd1ec17a2c8a9
Your Name <you@example.com>
2020-02-11 02:07:47 +0530

    Initial commit  i will upgrade it later on

---
+++ app/src/main/java/com/smartherd/recyclerviewbasics/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.smartherd.recyclerviewbasics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val item = ArrayList<Custom_class>()
+        item.add(Custom_class("Android App", R.drawable.background_two))
+        item.add(Custom_class("Android App", R.drawable.background_two))
+        item.add(Custom_class("Android App", R.drawable.background_two))
+        item.add(Custom_class("Android App", R.drawable.background_two))
+        item.add(Custom_class("Android App", R.drawable.background_two))
+        item.add(Custom_class("Android App", R.drawable.background_two))
+
+        val adapter = Adapter(item, applicationContext)
+        recycler_data.layoutManager = GridLayoutManager(applicationContext, 3)
+        recycler_data.adapter = adapter
+    }
+}
